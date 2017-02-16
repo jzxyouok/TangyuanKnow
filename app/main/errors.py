9b10bc952352360fb3cb687c81d2d6d0286356d1
bllli <me@lidengming.com>
2017-02-16 16:15:42 +0800

    add 错误页面

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,16 +1,21 @@
 from . import main
 from flask_wtf.csrf import CSRFError
-from flask import redirect, url_for
+from flask import redirect, url_for, render_template
+
+
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
 
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return 'Page not found', 404
+    return render_template('404.html'), 404
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return 'Internal server error', 500
+    return render_template('500.html'), 500
 
 
 @main.app_errorhandler(CSRFError)
