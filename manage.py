293842627f13c34fb70c669d50a042a33a21bd05
bllli <me@lidengming.com>
2017-04-23 11:54:28 +0800

    add manage shell增加了VRole类

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-from app.models import User, Role, Vote, Question, Answer
+from app.models import User, Role, Vote, Question, Answer, VRole
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -10,7 +10,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role,
+    return dict(app=app, db=db, User=User, Role=Role, VRole=VRole,
                 Vote=Vote, Question=Question, Answer=Answer)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
